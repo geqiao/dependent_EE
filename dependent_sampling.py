8727d41e6f4bd8a4ad91cb009fb85045e14c55ce
geqiao <geqiao@gmail.com>
2022-12-30 01:16:51 +0100

    feat: add generation of dependent sample using radical design

--- dependent_sampling.py
+++ dependent_sampling.py
@@ -12,19 +12,20 @@ def original_dist(z, type, a, b):
             z_cdf = sp.stats.norm.cdf(z)
             z_cdf[z_cdf == 1] = 1 - 1e-6
             z_cdf[z_cdf == 0] = 1e-6
-            z_origin = sp.stas.norm.ppf(z_cdf)
+            z_origin = sp.stats.norm.ppf(z_cdf)
     elif type == "uniform":
         z_origin = a + (b - a) * sp.stats.norm.cdf(z)
     return z_origin
 
 
-def correlated_trajectory(
+def generate_dependent_sample(
     k: int,
     data_file: str,
     corrcoef: np.ndarray,
     type: str,
     a: np.ndarray,
     b: np.ndarray,
+    design: str = "quasi_ot",
 ) -> pd.DataFrame:
     data = pd.read_csv(data_file, header=0)
     col_names = data.columns
@@ -51,16 +52,27 @@ def correlated_trajectory(
 
     Z_indep = sp.stats.norm.ppf(data.values)
     Out_Z_cor = []
-    index = np.ones((2 * k + 2), dtype=int)
-    index[0::2] = np.arange(k + 1)
-    index[1::2] = np.arange(k + 1)
-    index = index[1:-1]
+    if design == "quasi_ot":
+        index = np.ones((2 * k + 2), dtype=int)
+        index[1::2] = np.arange(k + 1)
+        index[0::2] = np.arange(k + 1)
+        index = index[1:-1]
+    elif design == "radical":
+        index = np.ones(2 * k, dtype=int)
+        index[0::2] = 0
+        index[1::2] = index[1::2].cumsum()
+
     for temp_Z_indep in Z_indep.reshape(-1, k + 1, k):
         temp_Z_indep = temp_Z_indep[index, :]
         out_temp_Z_dep = []
         out_temp_Z_dep_total = []
         for j in range(k):
-            cc = np.argwhere(temp_Z_indep[2 * j + 1, :] - temp_Z_indep[2 * j, :])[0][0]
+
+            cc = (
+                np.argwhere(temp_Z_indep[2 * j + 1, :] - temp_Z_indep[2 * j, :])[0][0]
+                if design == "quasi_ot"
+                else j
+            )
             if cc == k - 1:
                 if len(out_temp_Z_dep) == 0:
                     out_temp_Z_dep = temp_Z_indep[2 * j : 2 * j + 2, :].dot(U[0])
